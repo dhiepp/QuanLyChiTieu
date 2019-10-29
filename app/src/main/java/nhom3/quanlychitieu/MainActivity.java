1259573c1204c683e7ee992943defb672443f676
dhiep <dhiepp@users.noreply.github.com>
2019-10-30 00:12:18 +0700

    phục hồi

--- app/src/main/java/nhom3/quanlychitieu/MainActivity.java
+++ app/src/main/java/nhom3/quanlychitieu/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DBConnection.xuLiSaoChepSQL(MainActivity.this);
         initView();
 
     }
