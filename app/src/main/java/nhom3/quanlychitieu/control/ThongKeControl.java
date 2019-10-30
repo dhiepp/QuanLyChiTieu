80b9f5953616af6d4ddba91b1b1e30be19d0de53
NGUYEN NGOC CUONG <cuong001998@gmai.com>
2019-10-30 23:32:54 +0700

    cuong thongke

--- app/src/main/java/nhom3/quanlychitieu/control/ThongKeControl.java
+++ app/src/main/java/nhom3/quanlychitieu/control/ThongKeControl.java
@@ -1,4 +1,85 @@
 package nhom3.quanlychitieu.control;
 
-public class ThongKeControl {
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import nhom3.quanlychitieu.MainActivity;
+import nhom3.quanlychitieu.R;
+import nhom3.quanlychitieu.view.ThongKeFragment;
+
+public class ThongKeControl implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+
+    RadioGroup rb;
+    RadioButton rb1,rb2;
+    Button b1,b2;
+    LinearLayout ln;
+    TextView tv1,tv2;
+    Context context;
+
+    int year, month, dayOfMonth;
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
+
+    public ThongKeControl(RadioGroup rb,LinearLayout ln,Button b1,Button b2,TextView tv1, TextView tv2,Context context){
+        this.rb = rb;
+        this.ln = ln;
+        this.b1 = b1;
+        this.b2=b2;
+        this.tv1 = tv1;
+        this.tv2 = tv2;
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        calendar = Calendar.getInstance();
+        year= calendar.get(Calendar.YEAR);
+        month=calendar.get(Calendar.MONTH);
+        dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+       switch (view.getId()){
+           case R.id.btnStart :
+               datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                   @Override
+                   public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        tv1.setText(day+"-"+(month+1)+"-"+year);
+                   }
+               },year,month,dayOfMonth);
+               datePickerDialog.show();
+               break;
+           case R.id.btnEnd :
+               datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                   @Override
+                   public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                       tv2.setText(day+"-"+(month+1)+"-"+year);
+                   }
+               },year,month,dayOfMonth);
+               datePickerDialog.show();
+               break;
+       }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        if(i==R.id.rb_all){
+            ln.setVisibility(View.GONE);
+
+        }else{
+            ln.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+//    public void radioGroupChange(){
+//
+//    }
 }
--- app/src/main/java/nhom3/quanlychitieu/view/ThongKeFragment.java
+++ app/src/main/java/nhom3/quanlychitieu/view/ThongKeFragment.java
@@ -1,23 +1,41 @@
 package nhom3.quanlychitieu.view;
 
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.material.tabs.TabLayout;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import nhom3.quanlychitieu.R;
+import nhom3.quanlychitieu.control.ThongKeControl;
 
 public class ThongKeFragment extends Fragment {
     private Context context;
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
+
+    private Button btStart, btEnd;
+    private LinearLayout ln;
+    private RadioGroup rg;
+    private RadioButton rbAll, rbDate;
+    private ThongKeControl thongKeControl;
+    private TextView tv1, tv2;
+
+    int year, month, dayOfMonth;
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
 
     public ThongKeFragment() {
+
     }
 
     @Override
@@ -27,8 +45,43 @@ public class ThongKeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_thong_ke, container, false);
         context = root.getContext();
 
+        ln= root.findViewById(R.id.bg_linear);
+        btStart = root.findViewById(R.id.btnStart);
+        btEnd = root.findViewById(R.id.btnEnd);
+        rbAll = root.findViewById(R.id.rb_all);
+
+        rbDate = root.findViewById(R.id.rb_time);
+        rg = root.findViewById(R.id.rg);
+        tv1 = root.findViewById(R.id.tvStart);
+        tv2 = root.findViewById(R.id.tvEnd);
+
+        thongKeControl = new ThongKeControl(rg,ln,btStart,btEnd,tv1,tv2,context);
+        rg.setOnCheckedChangeListener(thongKeControl);
+        btStart.setOnClickListener(thongKeControl);
+        btEnd.setOnClickListener(thongKeControl);
+
         return root;
     }
 
 
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        rbAll.setChecked(true);
+//        //rbDate.setChecked(false);
+//    }
+
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        rbAll.setChecked(true);
+//    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        rbAll.setChecked(true);
+    }
 }
